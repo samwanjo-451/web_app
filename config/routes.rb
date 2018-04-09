6f3b74ff9637a8d2aefd59140d2f64c5ec303112
samuel wanjohi <samwanjo41@yahoo.com>
2018-04-09 10:44:29 +0300

    Added a home view, controller action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :lists, only: [:create, :destroy] do
-    resources :tasks, except: [:new, :edit, :show]
-  end
-
-  get 'environment', to: 'environment#index'
-
-  root "tasks#index"
+ 
+ 
+  
+  
+  root 'welcome#index'
 end
