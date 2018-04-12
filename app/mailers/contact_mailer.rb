d70b015f1f90ef2a3ecf7bafc588ad677ee4bb4f
samuel wanjohi <samwanjo41@yahoo.com>
2018-04-12 09:54:53 +0300

    Added contact form mailer view

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'instructors@upskillcourses.com'
+    default to: 'hulahoop41@gmail.com'
     def contact_email(name, email, body)
       @name = name
       @email = email
