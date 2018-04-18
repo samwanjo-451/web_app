7a87b0b48b59687ed2cf3c087228ce59c44dda29
samuel wanjohi <samwanjo41@yahoo.com>
2018-04-18 10:48:33 +0300

    Added server side code for creating subscriptions for pro users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  
+  protect_from_forgery 
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:stripe_card_token, :email, :password, :password_confirmation) }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    
-  devise_for :users
   root 'welcome#index'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'about', to: 'welcome#about'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
